9265c75b294913a8002ff38ed04238f8b75ee20b
dusans <39455139+dusm4n@users.noreply.github.com>
2023-03-05 21:01:41 +0100

    - Initial commit

---
+++ counter-vue/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <div>
+      <h3>
+        The current count is...
+      </h3>
+      <h1>{{count}}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: aquamarine;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+
+</style>
